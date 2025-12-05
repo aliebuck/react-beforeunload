8add46f681c03e88dd294d151940358911d9b554
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-05 15:31:18 +1300

    chore: upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import configPrettier from "eslint-config-prettier/flat";
+import pluginJsdoc from "eslint-plugin-jsdoc";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginVitest from "eslint-plugin-vitest";
+import globals from "globals";
+
+export default defineConfig([
+  globalIgnores(["coverage", "dist"]),
+  js.configs.recommended,
+  pluginJsdoc.configs['flat/recommended'],
+  pluginReactHooks.configs.flat["recommended-latest"],
+  configPrettier,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/__tests__/**/*.js"],
+    extends: [pluginVitest.configs.recommended],
+  },
+]);
